dce964092392c88a64b762d25c95c9ec4b16aa41
B_Attomic <123099609+BAttomic@users.noreply.github.com>
2024-05-03 18:01:02 -0300

    Add files via upload

---
+++ ADS100 - Programação de Computadores/Atividades/Exercicio número aleatório/Random_Number_Generador.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int numeroAleatorio, chute, tentativas = 0;
+    srand(time(0)); // Inicializa o gerador de números aleatórios
+
+    // Gera um número aleatório entre 1 e 100
+    numeroAleatorio = rand() % 100 + 1;
+
+    printf("Tente adivinhar o numero entre 1 e 100.\n");
+
+    // Loop para permitir que o usuário faça múltiplos chutes
+    do {
+        printf("Digite seu chute: ");
+        scanf("%d", &chute);
+        tentativas++;
+
+        // Verifica se o chute está abaixo do número
+        if (chute < numeroAleatorio) {
+            printf("Seu chute está abaixo do numero.\n");
+        }
+        // Verifica se o chute está acima do número
+        else if (chute > numeroAleatorio) {
+            printf("Seu chute está acima do numero.\n");
+        }
+        // Se não está abaixo nem acima, então o chute está correto
+        else {
+            printf("Parabens! Voce acertou o número em %d tentativas!\n", tentativas);
+        }
+    } while (chute != numeroAleatorio);
+
+    getch();
+    return 0;
+}
